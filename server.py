4dc7027f1f7becaec70c10fcf6dbf15ccd8640ef
fred.aponte <wilfredo.aponte@upr.edu>
2019-12-10 01:15:40 -0400

    external server implementation

--- server.py
+++ server.py
@@ -1,5 +1,7 @@
 import socket
 import random as rand
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
 extPort = 80
 ip = socket.gethostbyname('www.google.com')
 
@@ -26,9 +28,35 @@ def local_server():
 
 def rand_msg():
     msg = [b'The first computer programmer was a woman', b'The first computer bug was named after a real bug',
-           b'The first digital computer game never made any money']
+           b'The first digital computer game never made any money', b'The first electronic computer ENIAC weighed more than 27 tons and took up 1800 square feet.',
+           b'In September 1956 IBM launched the 305 RAMAC, the first (SUPER) computer with a hard disk drive (HDD). The HDD weighed over a ton and stored 5 MB of data.',
+           b'The first microprocessor created by Intel was the 4004. It was designed for a calculator, and in that time nobody imagined where it would lead.',
+           b'There is a higher chance of you getting killed by wolves than having an SHA-1 collision in Git', b'All Turing-complete programming languages are equally powerful,'
+           b'What many think of as formats are actually Turing-complete programming languages, e.g. Postscript and TeX']
 
-    return msg[rand.randrange(2)]
+    return msg[rand.randrange(8)]
 
 
-# def local_site():
+def local_site():
+    httpd = HTTPServer(('localhost', 8000), Server)
+    httpd.serve_forever()
+
+
+class Server(BaseHTTPRequestHandler):  # Part1 Local server
+    def do_GET(self):
+        if self.path == '/':
+            self.path = '/index.html'
+        # temp = input("Do you want to quit?")
+        # if temp == 'yes':
+        #     self.finish()
+        #     return
+        try:
+            file_to_open = open(self.path[1:]).read()
+            self.send_response(200)
+        except:
+            file_to_open = "ERROR: File not found"
+            print(file_to_open)
+            self.send_response(404)
+        self.end_headers()
+        self.wfile.write(bytes(file_to_open, 'utf-8'))
+
