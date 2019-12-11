833d73327b9c918acae123d005aef3c8b09fb973
fred.aponte <wilfredo.aponte@upr.edu>
2019-12-11 02:36:04 -0400

    Modified server and sockets

--- server.py
+++ server.py
@@ -31,29 +31,33 @@ def rand_msg():
            b'In September 1956 IBM launched the 305 RAMAC, the first (SUPER) computer with a hard disk drive (HDD). The HDD weighed over a ton and stored 5 MB of data.',
            b'The first microprocessor created by Intel was the 4004. It was designed for a calculator, and in that time nobody imagined where it would lead.',
            b'There is a higher chance of you getting killed by wolves than having an SHA-1 collision in Git', b'All Turing-complete programming languages are equally powerful,'
-           b'What many think of as formats are actually Turing-complete programming languages, e.g. Postscript and TeX']
+           b'What many think of as formats are actually Turing-complete programming languages, e.g. Postscript and TeX',
+           b'The language name C because it succeeds another language called B.', b'Programmers will start the count from zero, not one.',
+           b'The first actual computer "bug" was a one death moth that stuck in Harward mark II in 1947.', b'Doug Engelbart invented the first computer mouse in 1964. It was made using wood.',
+           b'HP, Microsoft and Apple have one very interesting thing in common, they were all started in a garage.', b'TYPEWRITER, this is the longest word that you can type only using first row of alphabets in your keyboard.',
+           b'You know that guys the average computer user blinks 7 times a minute, less than half the normal rate of 20.', b'The first electronic computer ENIAC weighed more than 27 tons and took up 1800 square feet']
 
-    return msg[rand.randrange(8)]
+    return msg[rand.randrange(16)]
 
 
 # Function opens localhost:8000 in order to host a HTML page
 def local_site():
-    httpd = HTTPServer(('localhost', 8000), Server)
+    httpd = HTTPServer(('localhost', 8000), BaseServer)
     httpd.serve_forever()
 
 
-# Class create local server
-class Server(BaseHTTPRequestHandler):
+# Class creates local server
+class BaseServer(BaseHTTPRequestHandler):
     def do_GET(self):
         if self.path == '/':
             self.path = '/index.html'
         try:
-            file_to_open = open(self.path[1:]).read()
+            open_file = open(self.path[1:]).read()
             self.send_response(200)
         except:
-            file_to_open = "ERROR: File not found"
-            print(file_to_open)
+            open_file = "ERROR: File not found"
+            print(open_file)
             self.send_response(404)
         self.end_headers()
-        self.wfile.write(bytes(file_to_open, 'utf-8'))
+        self.wfile.write(bytes(open_file, 'utf-8'))
 
--- sockets.py
+++ sockets.py
@@ -9,7 +9,7 @@ testPort = 65432
 def fact_client():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((testHost, testPort))
-        s.sendall(b'Hello, world')
+        s.sendall(b'Give me a fact!')
         data = s.recv(1024).decode()
 
     print('Fact from server: ' + data)
@@ -21,15 +21,16 @@ def client_chat():
     time.sleep(1)
     # Get the hostname, IP Address from socket and set Port
     s = socket.socket()
-    shost = socket.gethostname()
-    ip = socket.gethostbyname(shost)
-    # get information to connect with the server
-    print(shost, '({})'.format(ip))
+    s_host = socket.gethostname()
+    ip = socket.gethostbyname(s_host)
+    # Get information to connect with the server
+    print(s_host, '({})'.format(ip))
     server_host = input('Enter server\'s IP address:')
     name = input('Enter Client\'s name: ')
     port = 1234
     print('Trying to connect to the server: {}, ({})'.format(server_host, port))
     time.sleep(1)
+    # Connects socket with ip and port number
     s.connect((server_host, port))
     print("Connected...\n")
     s.send(name.encode())
@@ -37,6 +38,7 @@ def client_chat():
     server_name = server_name.decode()
     print('{} has joined...'.format(server_name))
     print('Enter exit() to exit.')
+    # Opens chat with client
     while True:
         msg = s.recv(1024)
         msg = msg.decode()
@@ -60,21 +62,23 @@ def server_chat():
     host_name = socket.gethostname()
     ip = socket.gethostbyname(host_name)
     port = 1234
-
+    # Binds ip and port to connect to
     soc.bind((host_name, port))
     print(host_name, '({})'.format(ip))
     name = input('Enter name: ')
-    soc.listen(1) # Try to locate using socket
+    # Locating using socket
+    soc.listen(1)
     print('Waiting for incoming connections...')
     connection, addr = soc.accept()
     print("Received connection from ", addr[0], "(", addr[1], ")\n")
     print('Connection Established. Connected From: {}, ({})'.format(addr[0], addr[0]))
-    # Get a connection from client side
+    # Get connection from client side
     client_name = connection.recv(1024)
     client_name = client_name.decode()
     print(client_name + ' has connected.')
     print('Enter exit() to leave the chat room')
     connection.send(name.encode())
+    # Opens chat with client
     while True:
         msg = input('Me > ')
         if msg == 'exit()':
